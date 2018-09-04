da877a8017e69d89ef72f30afb6dbf4d9aee344b
21jun <21jun7654@gmail.com>
2018-09-04 23:34:39 +0900

    stat parser

---
+++ StatParser.py
@@ -0,0 +1,98 @@
+import odbc
+from datetime import datetime
+import requests
+from bs4 import BeautifulSoup
+
+
+def date_pass():
+    now = datetime.now()
+    result = "%s-%s-%s %s:%s:%s" % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    return result
+
+
+def get_steam_users(steam_users):
+    users_cnt = []
+    for i in steam_users:
+        if i.string.replace(',', '').isdigit():
+            users_cnt.append(i.string.replace(',', ''))
+    return users_cnt
+
+
+def get_id(link):
+    _id = link.split('/')
+    if _id[2] == 'store.steampowered.com':
+        return _id[3], _id[4], _id[5]
+    elif _id[2] == 'steamcommunity.com':
+        return _id[3], _id[4]
+    else:
+        print('ERROR IN LINK')
+
+
+# db connection
+connect = odbc.odbc('oasis')
+db = connect.cursor()
+sql = '''
+    INSERT INTO oasis.player_count(title, ranking, date, current_players, peak_today, type, id_title, id_num, concurrent_steam_users, peak_steam_users) VALUES ("%s","%d","%s","%d","%d","%s","%s","%s","%d","%d")
+    '''
+sql_2 = '''
+    INSERT INTO oasis.player_count(title, ranking, date, current_players, peak_today, type, id_num, concurrent_steam_users, peak_steam_users) VALUES ("%s","%d","%s","%d","%d","%s","%s","%d","%d")
+    '''
+
+url = 'https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics'
+req = requests.get(url)
+html = req.text
+soup = BeautifulSoup(html, 'html.parser')
+
+steam_users = soup.select(
+    'body > div > div > div > div > div.page_content > div > div > div > div > table > tr'
+)
+
+data = soup.select(
+    '#detailStats > table > tr.player_count_row '
+)
+
+games = []
+date = date_pass()
+
+for index, i in enumerate(data):
+    link = i.find_all('a')[0].get('href')
+    title = i.find_all('a')[0].string
+    players = i.find_all('span')
+    current_players = players[0].string.replace(',', '')
+    peak_today = players[1].string.replace(',', '')
+    concurrent_steam_users, peak_steam_users = get_steam_users(steam_users[1])
+    id_tuple = get_id(link)
+    game = {
+        'current_players': int(current_players),
+        'peak_today': int(peak_today),
+        'title': title,
+        'date': date,
+        'ranking': int(index + 1),
+        'concurrent_steam_users': int(concurrent_steam_users),
+        'peak_steam_users': int(peak_steam_users)
+    }
+    if len(id_tuple) == 3:
+        game['type'] = id_tuple[0]
+        game['id_num'] = id_tuple[1]
+        game['id_title'] = id_tuple[2]
+    elif len(id_tuple) == 2:
+        game['type'] = id_tuple[0]
+        game['id_num'] = id_tuple[1]
+
+    print(game)
+    try:
+        db.execute(sql % (
+            game['title'], game['ranking'], game['date'],
+            game['current_players'], game['peak_today'],
+            game['type'], game['id_title'], game['id_num'],
+            game['concurrent_steam_users'], game['peak_steam_users']
+        ))
+    except ValueError:
+        print("ValueError at SQL INSERT")
+    except KeyError:
+        db.execute(sql_2 % (
+            game['title'], game['ranking'], game['date'],
+            game['current_players'], game['peak_today'],
+            game['type'], game['id_num'],
+            game['concurrent_steam_users'], game['peak_steam_users']
+        ))
--- test.py
+++ test.py
@@ -4,181 +4,20 @@ from time import sleep
 import requests
 from bs4 import BeautifulSoup
 
-
-def getReviews(info):
-    # print(info)
-    result = info.replace('\t', '').replace('\r', '').split('\n')
-    while ('' in result):
-        result.remove('')
-    print(result)
-
-    if ('Recent' in result[0]):
-
-        idx = 1
-
-        if (cleanNum(result[2]).isdigit() == False):
-            print("False")
-            idx = 0
-            recent_review_num = 0
-            recent_review = result[1]
-        else:
-            recent_review_num = cleanNum(result[idx + 1])
-            recent_review = result[1]
-
-        recent_review_percentage = '0%'
-        a = result[idx + 2].split(' ')
-        for col in a:
-            if ('%' in col):
-                recent_review_percentage = col
-
-        all_review_percentage = '0%'
-        a = result[idx + 6].split(' ')
-        for col in a:
-            if ('%' in col):
-                all_review_percentage = col
-
-        return {
-            'recent_review': recent_review,
-            'recent_review_num': recent_review_num,
-            'recent_review_percentage': recent_review_percentage,
-            'all_review': result[idx + 4],
-            'all_review_num': cleanNum(result[idx + 5]),
-            'all_review_percentage': all_review_percentage
-
-        }
-    else:
-        if ('No' in result[1]):
-            return {
-                'recent_review': 'NONE',
-                'recent_review_num': 0,
-                'recent_review_percentage': '0%',
-                'all_review': 'NONE',
-                'all_review_num': 0,
-                'all_review_percentage': '0%'
-            }
-
-        all_review_percentage = '0%'
-        a = result[3].split(' ')
-        for col in a:
-            if ('%' in col):
-                all_review_percentage = col
-
-        return {
-            'recent_review': 'NONE',
-            'recent_review_num': 0,
-            'recent_review_percentage': '0%',
-            'all_review': result[1],
-            'all_review_num': cleanNum(result[2]),
-            'all_review_percentage': all_review_percentage
-        }
-def getTags(tags):
-    try:
-        result = tags.replace('\t', '')
-        result = result.replace('\r', '')
-        result = result.replace('\n', ' ')
-        result = result.replace('+', '')
-        result = result.replace('  ', '')
-        result = result.split(' ')
-        result.remove('')
-    except:
-        print("EXCEPT")
-    return result
-
-
-def cleanStr(str):
-    result = str.replace('\t', '')
-    result = result.replace('\r', '')
-    result = result.replace('\n', ' ')
-    # result = result.replace('  ', '')
-    # result = result.replace(' ','')
-    # result = result.split(' ')
-    return result
-
-
-def cleanNum(str):
-    result = str.replace('(', '')
-    result = result.replace(')', '')
-    result = result.replace(',', '')
-    return result
-
-
-id_title = ''
-id_num = '740080'
-type = 'app'
-# {'id_title': 'They_Are_Billions', 'id_num': '644930', 'recent_review': 'Mostly Positive', 'recent_review_num': 0, 'recent_review_percentage': '78%', 'all_review': 'No user reviews', 'all_review_num': 'Release Date:', 'all_review_percentage': '0%', 'developer': 'Numantian Games', 'publisher': 'Numantian Games', 'tag': 'Early,Access,Base,Building,Strategy,Survival,Zombies,RTS,Steampunk,Post-apocalyptic,City,Builder,Building,Tower,Defense,Singleplayer,Resource,Management,Real-Time,with,Pause,Early,Access,Tactical,Difficult,Management,Indie,Isometric'}
-url = 'https://store.steampowered.com/' + str(type) + '/' + str(id_num) + '/' + str(id_title)
+url = 'https://store.steampowered.com/stats/Steam-Game-and-Player-Statistics'
 req = requests.get(url)
 html = req.text
 soup = BeautifulSoup(html, 'html.parser')
 
-
-#print(soup)
-
-ageCheck = soup.select(
-    '#agecheck_form > h2'
+total_players = soup.select(
+    'body > div > div > div > div > div.page_content > div > div > div > div > table > tr'
 )
 
-contentWarning = soup.select(
-    '#app_agegate > div > h2'
-)
 
-tags = soup.select(
-    '#game_highlights > div > div > div > div > div.glance_tags.popular_tags'
-)
-# developers_list > a
-developer = soup.select(
-    '#developers_list > a'
-)
-
-publisher = soup.select(
-    '#game_highlights > div > div > div > div > div > div.summary.column > a'
-)
-
-recent_review = soup.select(
-    '#game_highlights > div > div > div > div > div > div > span'
-)
-
-# game_highlights > div.rightcol > div > div.glance_ctn_responsive_left > div
-
-info = soup.select(
-    '#game_highlights > div > div > div.glance_ctn_responsive_left > div '
-)
-
-try:
-    information = info[0]
-except IndexError:
-    print("INDEX OUT OF RANGE")
-    quit()
-# print(information)
-a = getReviews(information.text)
-#print(a)
-
-game = {}
-game['id_title'] = id_title
-game['id_num'] = id_num
-game['recent_review'] = a['recent_review']
-game['recent_review_num'] = a['recent_review_num']
-game['recent_review_percentage'] = a['recent_review_percentage']
-game['all_review'] = a['all_review']
-game['all_review_num'] = a['all_review_num']
-game['all_review_percentage'] = a['all_review_percentage']
-if (developer):
-    print((developer[0].text))
-    game['developer'] = developer[0].text
-else:
-    game['developer'] = 'NONE'
-if (publisher and len(publisher) > 1):
-    print(publisher[1].text)
-    game['publisher'] = publisher[1].text
-else:
-    game['publisher'] = 'NONE'
-if (tags):
-    print(getTags(tags[0].text))
-    _tags = ','.join(getTags(tags[0].text))
-    game['tag'] = _tags
-else:
-    game['tag'] = 'NONE'
-print("----------------------------------")
+def aaa():
+    a = ["f", "s","fs"]
+    return a
 
 
-print(game)
+aa, bb, cc = aaa()
+print(aa,bb,cc)
